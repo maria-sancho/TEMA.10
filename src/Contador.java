35ec6cbafec65d6218e8bed554739e0f4fd77afe
maria-sancho <alusan7934@ieselcaminas.org>
2023-05-12 00:13:45 +0200

    TEMA10.

--- src/Contador.java
+++ src/Contador.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class Contador {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Contador {
+
+    public static void main(String[] args) {
+        File archivo = new File("carta.txt");
+        int contadorCaracteres = 0;
+        int contadorLineas = 0;
+        int contadorPalabras = 0;
+        try {
+            Scanner lector = new Scanner(archivo);
+            while (lector.hasNextLine()) {
+                String linea = lector.nextLine();
+                contadorLineas++;
+                contadorCaracteres += linea.length();
+                contadorPalabras += contarPalabras(linea);
+            }
+            lector.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("El archivo no se ha encontrado.");
+        }
+        System.out.println("Caracteres: " + contadorCaracteres);
+        System.out.println("Lineas: " + contadorLineas);
+        System.out.println("Palabras: " + contadorPalabras);
+    }
+
+    private static int contarPalabras(String linea) {
+        int contador = 0;
+        boolean enPalabra = false;
+        for (int i = 0; i < linea.length(); i++) {
+            char c = linea.charAt(i);
+            if (c == ' ' || c == '\n') {
+                if (enPalabra) {
+                    contador++;
+                    enPalabra = false;
+                }
+            } else {
+                enPalabra = true;
+            }
+        }
+        if (enPalabra) {
+            contador++;
+        }
+        return contador;
+    }
 }
--- src/GuardarDatos.java
+++ src/GuardarDatos.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class GuardarDatos {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class GuardarDatos {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce tu nombre: ");
+        String nombre = scanner.nextLine();
+        System.out.print("Introduce tu edad: ");
+        int edad = scanner.nextInt();
+
+        File archivo = new File("datos.txt");
+        try {
+            FileWriter escritor = new FileWriter(archivo, true);
+            escritor.write(nombre + "," + edad + "\n");
+            escritor.close();
+            System.out.println("Los datos se han guardado correctamente.");
+        } catch (IOException e) {
+            System.out.println("Se ha producido un error al guardar los datos.");
+            e.printStackTrace();
+        }
+    }
 }
